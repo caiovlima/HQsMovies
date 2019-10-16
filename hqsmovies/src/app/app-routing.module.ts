ff7577cce12a09ab6946a7d19a366eff20668df5
Caio Vinicius De Lima <32105582+caiovlima@users.noreply.github.com>
2019-10-15 21:23:14 -0300

    Update app-routing.module.ts

--- hqsmovies/src/app/app-routing.module.ts
+++ hqsmovies/src/app/app-routing.module.ts
@@ -6,9 +6,10 @@ import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // { path: 'login', component: LoginComponent },
+  // { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
 
 @NgModule({
